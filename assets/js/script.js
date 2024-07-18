5243313e11f5079c97092cdc26c411acd2b18aa4
Sayandeep <sayandeep11@gmail.com>
2024-07-18 06:36:33 +0530

    navbar fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-'use strict';
+// 'use strict';
 
-/**
- * navbar variables
- */
+// /**
+//  * navbar variables
+//  */
 
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-const header = document.querySelector("[data-header]");
+// const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+// const header = document.querySelector("[data-header]");
 
-navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("active");
-});
+// navToggleBtn.addEventListener("click", function () {
+//   header.classList.toggle("active");
+// });
 
 
